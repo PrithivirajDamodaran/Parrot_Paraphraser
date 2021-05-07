4e77bcf9dceedf8776cb2789ba4210b17ec1f6ac
Prithivida <d.prithiviraj@gmail.com>
2021-05-07 15:20:44 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='parrot',
+    version='1.0',
+    description='Parrot paraphraser',
+    url='git@github.com:PrithivirajDamodaran/Parrot.git',
+    author='Prithiviraj Damodaran',
+    author_email='d.prithiviraj@gmail.com',
+    license='Apache 2.0',
+    packages=[parrot],
+    zip_safe=False
+)
